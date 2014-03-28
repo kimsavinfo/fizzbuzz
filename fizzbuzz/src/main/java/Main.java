5a5098b9d10b59b6b0e7c9d6abcedfac1d4f2cbf
kimsavinfo <kimsavinfo@gmail.com>
2014-03-28 15:39:08 +0100

    before

--- fizzbuzz/src/main/java/Main.java
+++ fizzbuzz/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import static spark.Spark.*;
+import spark.*;
+
 /**
  * Created by kimsavinfo on 28/03/14.
  */
@@ -5,6 +8,14 @@ public class Main
 {
     public static void main (String [] arg)
     {
+        get(new Route("/fizzbuzz") {
+            @Override
+            public Object handle(Request request, Response response)
+            {
+                return "fizzbuzz";
+            }
+        });
+
         FizzBuzz fizzBuzz= new FizzBuzz();
 
         for (int i=1; i<=100; i++)
