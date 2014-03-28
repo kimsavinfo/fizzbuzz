5bfde92268dd06fc7ac0ebc9e09b6addcaee4526
kimsavinfo <kimsavinfo@gmail.com>
2014-03-28 15:17:19 +0100

    before

--- fizzbuzz/src/test/java/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -1,36 +1,42 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 /**
  * Created by kimsavinfo on 28/03/14.
  */
 public class FizzBuzzTest
 {
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void SetUp()
+    {
+        this.fizzBuzz = new FizzBuzz();
+    }
+
+
     @Test
     public void getResultShouldReturnNumber()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertEquals("1", fizzBuzz.getResult(1));
     }
 
     @Test
     public void getResultShouldReturnFizz()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertEquals("fizz", fizzBuzz.getResult(3));
     }
 
     @Test
     public void getResultShouldReturnBuzz()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertEquals("buzz", fizzBuzz.getResult(5));
     }
 
     @Test
     public void getResultShouldReturnFizzBuzz()
     {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assert.assertEquals("fizzbuzz", fizzBuzz.getResult(15));
     }
 
